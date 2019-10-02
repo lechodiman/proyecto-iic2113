3b07cba26aef2f25c2437f7118dce6c71fbf331b
Luis Chodiman <lechodiman@uc.cl>
2019-10-02 19:38:56 -0300

    Create Sponsor

---
+++ proyecto-iic2113/Data/Migrations/20191002223851_CreateSponsor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace proyecto_iic2113.Data.Migrations
+{
+    public partial class CreateSponsor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ proyecto-iic2113/Models/Sponsor.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace proyecto_iic2113.Models
+{
+    public class Sponsor
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
