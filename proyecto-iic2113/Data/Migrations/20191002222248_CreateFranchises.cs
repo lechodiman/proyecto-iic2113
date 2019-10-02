764b5f91c41ffc7de3da73ed8df27fa0503755e2
Luis Chodiman <lechodiman@uc.cl>
2019-10-02 19:22:55 -0300

    Create Franchise

---
+++ proyecto-iic2113/Data/Migrations/20191002222248_CreateFranchises.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace proyecto_iic2113.Data.Migrations
+{
+    public partial class CreateFranchises : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ proyecto-iic2113/Models/Franchise.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace proyecto_iic2113.Models
+{
+    public class Franchise
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name cannot be blank")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
