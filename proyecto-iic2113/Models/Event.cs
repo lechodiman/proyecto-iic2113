c1946e5d14a16ffc5418c712042266dfab2340c5
benjamimo1 <benjamimo1@MacBook-Space-Gray.local>
2019-10-02 19:20:44 -0300

    Modelos varios agregados

---
+++ proyecto-iic2113/Models/Event.cs
@@ -0,0 +1,10 @@
+public class Event {
+
+	public int Id{get;set;}
+	public string Name{get;set;}
+	public string Room{get;set;}
+	public string Datetime{get;set;}
+	public string Subject{get;set;}
+	public string Description{get;set;}
+
+}
