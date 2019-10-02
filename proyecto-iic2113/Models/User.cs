ca967bfe39310f39db9d5b21345da6e087084d49
benjamimo1 <benjamimo1@MacBook-Space-Gray.local>
2019-10-02 19:07:54 -0300

    users model

---
+++ proyecto-iic2113/Models/User.cs
@@ -0,0 +1,19 @@
+
+
+ public class User{
+
+ 	private string Name;
+ 	private int Id;
+ 	private string Mail;
+ 	private string PhoneNumber;
+
+ 	public User(string name, int id, string mail, string phoneNumber){
+
+ 		
+
+ 	}
+
+ 	private void Funcion(int a, int b){
+ 		return;
+ 	}
+ }
