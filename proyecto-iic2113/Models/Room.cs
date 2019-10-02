d08d783bcacb4ea1c4cf16bbb61ecb3dd547b30a
Luis Chodiman <lechodiman@uc.cl>
2019-10-02 19:39:50 -0300

    Create Room

---
+++ proyecto-iic2113/Models/Room.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace proyecto_iic2113.Models
+{
+    public class Room
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Photo { get; set; }
+    }
+}
