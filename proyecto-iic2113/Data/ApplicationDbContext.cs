890de03bcc0de3d1c498addbf343d6d82c8cb7fc
Luis Chodiman <lechodiman@uc.cl>
2019-10-02 19:46:05 -0300

    Import Models namespace and delete Chat DbSet

--- proyecto-iic2113/Data/ApplicationDbContext.cs
+++ proyecto-iic2113/Data/ApplicationDbContext.cs
@@ -1,22 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+
+using proyecto_iic2113.Models;
 
 namespace proyecto_iic2113.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
 
-    	public DbSet<Chat> Chats {get; set;}
-    	public DbSet<Event> Events {get; set;}
-    	public DbSet<Menu> Menus {get; set;}
-    	public DbSet<Room> Rooms {get; set;}
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Room> Rooms { get; set; }
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     }
 }
