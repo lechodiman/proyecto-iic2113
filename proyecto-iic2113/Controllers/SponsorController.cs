97933415b3095da8a4aa23b59839d7309680385d
Luis Chodiman <lechodiman@uc.cl>
2019-10-08 15:26:21 -0300

    Create Sponsors CRUD and Controller

---
+++ proyecto-iic2113/Controllers/SponsorController.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using proyecto_iic2113.Data;
+using proyecto_iic2113.Models;
+
+namespace proyecto_iic2113.Controllers
+{
+    public class SponsorController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SponsorController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Sponsor
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Sponsors.ToListAsync());
+        }
+
+        // GET: Sponsor/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sponsor = await _context.Sponsors
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (sponsor == null)
+            {
+                return NotFound();
+            }
+
+            return View(sponsor);
+        }
+
+        // GET: Sponsor/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sponsor/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Sponsor sponsor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(sponsor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sponsor);
+        }
+
+        // GET: Sponsor/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sponsor = await _context.Sponsors.FindAsync(id);
+            if (sponsor == null)
+            {
+                return NotFound();
+            }
+            return View(sponsor);
+        }
+
+        // POST: Sponsor/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Sponsor sponsor)
+        {
+            if (id != sponsor.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sponsor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SponsorExists(sponsor.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sponsor);
+        }
+
+        // GET: Sponsor/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sponsor = await _context.Sponsors
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (sponsor == null)
+            {
+                return NotFound();
+            }
+
+            return View(sponsor);
+        }
+
+        // POST: Sponsor/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var sponsor = await _context.Sponsors.FindAsync(id);
+            _context.Sponsors.Remove(sponsor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SponsorExists(int id)
+        {
+            return _context.Sponsors.Any(e => e.Id == id);
+        }
+    }
+}
