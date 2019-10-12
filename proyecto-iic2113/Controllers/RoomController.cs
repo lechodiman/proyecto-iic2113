f56d8cb4016328515bf4056a33bd34a7136af47c
GabrielLyonb <glyon1@uc.cl>
2019-10-12 20:23:26 -0300

    schaffold View and Controller: Room

---
+++ proyecto-iic2113/Controllers/RoomController.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using proyecto_iic2113.Data;
+using proyecto_iic2113.Models;
+
+namespace proyecto_iic2113.Controllers
+{
+    public class RoomController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RoomController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Room
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Rooms.Include(r => r.Venue);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Room/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Rooms
+                .Include(r => r.Venue)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+        // GET: Room/Create
+        public IActionResult Create()
+        {
+            ViewData["VenueId"] = new SelectList(_context.Venues, "Id", "Name");
+            return View();
+        }
+
+        // POST: Room/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Photo,VenueId")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(room);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["VenueId"] = new SelectList(_context.Venues, "Id", "Name", room.VenueId);
+            return View(room);
+        }
+
+        // GET: Room/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Rooms.FindAsync(id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+            ViewData["VenueId"] = new SelectList(_context.Venues, "Id", "Name", room.VenueId);
+            return View(room);
+        }
+
+        // POST: Room/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Photo,VenueId")] Room room)
+        {
+            if (id != room.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(room);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoomExists(room.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["VenueId"] = new SelectList(_context.Venues, "Id", "Name", room.VenueId);
+            return View(room);
+        }
+
+        // GET: Room/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Rooms
+                .Include(r => r.Venue)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+        // POST: Room/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var room = await _context.Rooms.FindAsync(id);
+            _context.Rooms.Remove(room);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RoomExists(int id)
+        {
+            return _context.Rooms.Any(e => e.Id == id);
+        }
+    }
+}
