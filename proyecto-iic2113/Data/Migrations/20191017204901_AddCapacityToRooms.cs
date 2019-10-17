771a06a3f1ea0282e0dd5f4862a5e3c05dd84ff9
Luis Chodiman <lechodiman@uc.cl>
2019-10-17 17:53:44 -0300

    :card_file_box: Add capacity to rooms

---
+++ proyecto-iic2113/Data/Migrations/20191017204901_AddCapacityToRooms.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace proyecto_iic2113.Migrations
+{
+    public partial class AddCapacityToRooms : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Capacity",
+                table: "Rooms",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Capacity",
+                table: "Rooms");
+        }
+    }
+}
