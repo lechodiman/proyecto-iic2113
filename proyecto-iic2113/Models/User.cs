5f637dfc101c4b7cb953ef078ae01dc41b7313c7
mathiasvaldebenito <mlvaldebenito@uc.cl>
2019-10-17 17:04:31 -0400

    Conferences at home

---
+++ proyecto-iic2113/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace proyecto_iic2113.Models
+{
+    public class User : IdentityUser
+    {
+        public string CustomTag { get; set; }
+        public IEnumerable<Conference> Conferences { get; set; }
+    }
+}
+
+
