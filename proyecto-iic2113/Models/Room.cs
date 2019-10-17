99b6f77a49068be714fd7f08d61966d6675aab22
Luis Chodiman <lechodiman@uc.cl>
2019-10-17 16:49:24 -0300

    :bug: Fix migrations syncronization bug

--- proyecto-iic2113/Models/Room.cs
+++ proyecto-iic2113/Models/Room.cs
@@ -10,11 +10,14 @@ namespace proyecto_iic2113.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
         public string Photo { get; set; }
+
         public int VenueId { get; set; }
         public Venue Venue { get; set; }
+
         public IEnumerable<Equipment> Equipments { get; set; }
     }
 }
