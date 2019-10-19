ebe1aeecaf61e17833a7a62e2163d94678987859
Luis Chodiman <lechodiman@uc.cl>
2019-10-19 20:13:16 -0300

    :sparkles: Workshop and Resources CRUD

---
+++ proyecto-iic2113/Controllers/ResourceController.cs
@@ -0,0 +1,162 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+using proyecto_iic2113.Data;
+using proyecto_iic2113.Models;
+
+namespace proyecto_iic2113.Controllers
+{
+    public class ResourceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ResourceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Resource
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Resources.Include(r => r.Workshop);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Resource/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resource = await _context.Resources
+                .Include(r => r.Workshop)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (resource == null)
+            {
+                return NotFound();
+            }
+
+            return View(resource);
+        }
+
+        // GET: Resource/Create
+        public IActionResult Create()
+        {
+            ViewData["WorkshopId"] = new SelectList(_context.Workshops, "Id", "Name");
+            return View();
+        }
+
+        // POST: Resource/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ResourceUrl,WorkshopId")] Resource resource)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(resource);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["WorkshopId"] = new SelectList(_context.Workshops, "Id", "Discriminator", resource.WorkshopId);
+            return View(resource);
+        }
+
+        // GET: Resource/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resource = await _context.Resources.FindAsync(id);
+            if (resource == null)
+            {
+                return NotFound();
+            }
+            ViewData["WorkshopId"] = new SelectList(_context.Workshops, "Id", "Discriminator", resource.WorkshopId);
+            return View(resource);
+        }
+
+        // POST: Resource/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ResourceUrl,WorkshopId")] Resource resource)
+        {
+            if (id != resource.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(resource);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ResourceExists(resource.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["WorkshopId"] = new SelectList(_context.Workshops, "Id", "Discriminator", resource.WorkshopId);
+            return View(resource);
+        }
+
+        // GET: Resource/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resource = await _context.Resources
+                .Include(r => r.Workshop)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (resource == null)
+            {
+                return NotFound();
+            }
+
+            return View(resource);
+        }
+
+        // POST: Resource/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var resource = await _context.Resources.FindAsync(id);
+            _context.Resources.Remove(resource);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ResourceExists(int id)
+        {
+            return _context.Resources.Any(e => e.Id == id);
+        }
+    }
+}
