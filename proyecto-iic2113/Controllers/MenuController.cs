a42c72388a2e240e899cff83c7527079d1ffc08b
Luis Chodiman <lechodiman@uc.cl>
2019-10-19 19:52:09 -0300

    :sparkles: Menu CRUD

---
+++ proyecto-iic2113/Controllers/MenuController.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using proyecto_iic2113.Data;
+using proyecto_iic2113.Models;
+
+namespace proyecto_iic2113.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MenuController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Menu
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Menus.Include(m => m.Launch);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Menu/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menus
+                .Include(m => m.Launch)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return View(menu);
+        }
+
+        // GET: Menu/Create
+        public IActionResult Create()
+        {
+            ViewData["LaunchId"] = new SelectList(_context.Set<Launch>(), "Id", "Discriminator");
+            return View();
+        }
+
+        // POST: Menu/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FoodName,IsVegan,LaunchId")] Menu menu)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(menu);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LaunchId"] = new SelectList(_context.Set<Launch>(), "Id", "Discriminator", menu.LaunchId);
+            return View(menu);
+        }
+
+        // GET: Menu/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menus.FindAsync(id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+            ViewData["LaunchId"] = new SelectList(_context.Set<Launch>(), "Id", "Discriminator", menu.LaunchId);
+            return View(menu);
+        }
+
+        // POST: Menu/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FoodName,IsVegan,LaunchId")] Menu menu)
+        {
+            if (id != menu.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(menu);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MenuExists(menu.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LaunchId"] = new SelectList(_context.Set<Launch>(), "Id", "Discriminator", menu.LaunchId);
+            return View(menu);
+        }
+
+        // GET: Menu/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menus
+                .Include(m => m.Launch)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return View(menu);
+        }
+
+        // POST: Menu/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var menu = await _context.Menus.FindAsync(id);
+            _context.Menus.Remove(menu);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MenuExists(int id)
+        {
+            return _context.Menus.Any(e => e.Id == id);
+        }
+    }
+}
