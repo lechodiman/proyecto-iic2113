0f88ea220d6930fc6d8b5d971920853097df5b8d
mathiasvaldebenito <mlvaldebenito@uc.cl>
2019-11-01 23:59:48 -0400

    migration addres required

---
+++ proyecto-iic2113/Data/Migrations/20191102035724_Addres_required.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace proyecto_iic2113.Migrations
+{
+    public partial class Addres_required : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Adress",
+                table: "Venues",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Adress",
+                table: "Venues",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
