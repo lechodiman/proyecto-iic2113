da9981a579b98bb16f66515ab7936bbb6d467b7c
Benja Munoz <bomunoz@uc.cl>
2019-11-09 22:01:34 -0300

    DINAMIC URL

--- proyecto-iic2113/Controllers/ConferenceController.cs
+++ proyecto-iic2113/Controllers/ConferenceController.cs
@@ -208,5 +208,12 @@ namespace proyecto_iic2113.Controllers
         }
 
         private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+        // [HttpGet]
+        // public IActionResult GetVenueController()
+        // {
+        //     var url = Url.Action("Details", "Venue");
+        //     return Ok(url);
+        // }
     }
 }
