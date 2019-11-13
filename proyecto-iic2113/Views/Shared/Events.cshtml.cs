9f90f2239ed8dcca2a5c1c94de436e71842cb6e6
mathiasvaldebenito <mlvaldebenito@uc.cl>
2019-11-12 21:55:01 -0300

    index view of all events

---
+++ proyecto-iic2113/Views/Shared/Events.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace proyecto_iic2113.Views.Shared
+{
+    public class EventsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
