03ceb26dadd80ce4019659046c60d0cf9e146a95
Luis Chodiman <lechodiman@uc.cl>
2019-11-21 18:06:34 -0300

    :sparkles: scaffold reviews controller and fix discriminator value

---
+++ proyecto-iic2113/Controllers/ReviewController.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+using proyecto_iic2113.Data;
+using proyecto_iic2113.Models;
+
+namespace proyecto_iic2113.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Review
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Reviews.Include(r => r.ApplicationUser).Include(r => r.Event);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Review/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews
+                .Include(r => r.ApplicationUser)
+                .Include(r => r.Event)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return View(review);
+        }
+
+        // GET: Review/Create
+        public IActionResult Create()
+        {
+            ViewData["ApplicationUserId"] = new SelectList(_context.Users, "Id", "Email");
+            ViewData["EventId"] = new SelectList(_context.Events, "Id", "Name");
+            return View();
+        }
+
+        // POST: Review/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Rating,Body,EventId,ApplicationUserId")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(review);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApplicationUserId"] = new SelectList(_context.Users, "Id", "Id", review.ApplicationUserId);
+            ViewData["EventId"] = new SelectList(_context.Events, "Id", "Name", review.EventId);
+            return View(review);
+        }
+
+        // GET: Review/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews.FindAsync(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+            ViewData["ApplicationUserId"] = new SelectList(_context.Users, "Id", "Email", review.ApplicationUserId);
+            ViewData["EventId"] = new SelectList(_context.Events, "Id", "Name", review.EventId);
+            return View(review);
+        }
+
+        // POST: Review/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Rating,Body,EventId,ApplicationUserId")] Review review)
+        {
+            if (id != review.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(review);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReviewExists(review.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApplicationUserId"] = new SelectList(_context.Users, "Id", "Email", review.ApplicationUserId);
+            ViewData["EventId"] = new SelectList(_context.Events, "Id", "Name", review.EventId);
+            return View(review);
+        }
+
+        // GET: Review/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews
+                .Include(r => r.ApplicationUser)
+                .Include(r => r.Event)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return View(review);
+        }
+
+        // POST: Review/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var review = await _context.Reviews.FindAsync(id);
+            _context.Reviews.Remove(review);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReviewExists(int id)
+        {
+            return _context.Reviews.Any(e => e.Id == id);
+        }
+    }
+}
