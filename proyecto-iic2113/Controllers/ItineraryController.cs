49caa8e4b7a1899368c142b9c73ab2d00c0b6a37
Benjamin Munoz-Palma <bomunoz@uc.cl>
2019-12-18 16:35:20 -0300

    Feature/itinerario (#34)
    * Primer funcional de itinerario
    
    * Conferencias con URL
    
    * Filtrado para solo mostrar las del usuario en cuestion
    
    * Se elimina print
    
    * Itinerario de eventos y conferencias
    
    * Url de my itinerary en head bar
    
    * Separacion por tipo de evento
    
    * Cambios para comentarios PR
    
    * Cambios para pr parte 2

---
+++ proyecto-iic2113/Controllers/ItineraryController.cs
@@ -0,0 +1,129 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using proyecto_iic2113.Data;
+using proyecto_iic2113.Models;
+using System;
+
+namespace proyecto_iic2113.Controllers
+{
+    public class ItineraryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+       
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+        public ItineraryController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+
+        // GET: Sponsor/Details/5
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            var currentUser = await GetCurrentUserAsync();
+        
+            var baseConferenceQuery = await _context.ConferenceUserAttendees
+                .Join(
+                    _context.Conferences,
+                    attendance => attendance.ConferenceId,
+                    conference => conference.Id,
+                    (attendance, conference) => new AttendanceView
+                    {
+                        ConferenceId = conference.Id,
+                        Name = conference.Name,
+                        Venue = conference.Venue.Name,
+                        EndDate = conference.EndDate,
+                        DateTime = conference.DateTime,
+                        ApplicationUserId = attendance.ApplicationUserId
+                        
+                    }
+                ).Where(a => a.ApplicationUserId == currentUser.Id).ToListAsync();
+
+            ViewBag.Conferences = baseConferenceQuery;
+
+
+            ViewBag.Parties = await _context.EventUserAttendees
+                .Join(
+                    _context.Parties,
+                    attendance => attendance.EventId,
+                    event_i => event_i.Id,
+                    (attendance, event_i) => new AttendanceViewEvent
+                    {
+                        EventId = event_i.Id,
+                        Name = event_i.Name,
+                        EndDate = event_i.EndDate,
+                        StartDate = event_i.StartDate,
+                        ApplicationUserId = attendance.ApplicationUserId
+
+
+                    }
+                ).Where(e => e.ApplicationUserId == currentUser.Id).ToListAsync();
+
+
+            ViewBag.Lunches = await _context.EventUserAttendees
+                .Join(
+                    _context.Launches,
+                    attendance => attendance.EventId,
+                    event_i => event_i.Id,
+                    (attendance, event_i) => new AttendanceViewEvent
+                    {
+                        EventId = event_i.Id,
+                        Name = event_i.Name,
+                        EndDate = event_i.EndDate,
+                        StartDate = event_i.StartDate,
+                        ApplicationUserId = attendance.ApplicationUserId
+
+
+                    }
+                ).Where(e => e.ApplicationUserId == currentUser.Id).ToListAsync();
+
+
+            ViewBag.Chats = await _context.EventUserAttendees
+                .Join(
+                    _context.Chat,
+                    attendance => attendance.EventId,
+                    event_i => event_i.Id,
+                    (attendance, event_i) => new AttendanceViewEvent
+                    {
+                        EventId = event_i.Id,
+                        Name = event_i.Name,
+                        EndDate = event_i.EndDate,
+                        StartDate = event_i.StartDate,
+                        ApplicationUserId = attendance.ApplicationUserId
+
+
+                    }
+                ).Where(e => e.ApplicationUserId == currentUser.Id).ToListAsync();
+
+            ViewBag.Talks = await _context.EventUserAttendees
+                .Join(
+                    _context.Talks,
+                    attendance => attendance.EventId,
+                    event_i => event_i.Id,
+                    (attendance, event_i) => new AttendanceViewEvent
+                    {
+                        EventId = event_i.Id,
+                        Name = event_i.Name,
+                        EndDate = event_i.EndDate,
+                        StartDate = event_i.StartDate,
+                        ApplicationUserId = attendance.ApplicationUserId
+
+
+                    }
+                ).Where(e => e.ApplicationUserId == currentUser.Id).ToListAsync();
+
+            return View();
+        }
+    }
+}
---
+++ proyecto-iic2113/Models/AttendanceView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace proyecto_iic2113.Models
+{
+    public class AttendanceView
+    {
+            public int ConferenceId { get; set; }
+            public string Name { get; set; }
+            public string Venue { get; set; }
+            public DateTime EndDate { get; set; }
+            public DateTime DateTime { get; set; }
+            public string ApplicationUserId { get; set; }
+    }
+}
